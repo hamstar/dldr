eec48431e1907dbc975f31915f8c790497eef5f4
hamstar <hamstar@behemoth>
2011-12-30 12:31:28 +1300

    added a central place for constants and file loading

---
+++ remote/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('BASE_PATH', '');
+define('RUN_PATH', '');
+define('DOWNLOAD_PATH', '');
+define('COOKIE_PATH', '');
+define('DATA_PATH', '');
+
+require 'exceptions.php';
+require 'models/cookie.php';
+require 'models/dldr.php';
+require 'models/url.php';
