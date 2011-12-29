125a6302c356e5fd623cf777a14a0b7674f347f1
Robert McLeod <hamstar@behemoth.(none)>
2011-12-30 03:03:48 +1300

    added run in background and restructured a little bit

--- remote/models/dldr.php
+++ remote/models/dldr.php
@@ -4,5 +4,36 @@ class Dldr {
 
 	function start( Url $url ) {
 		
+		$this->url = $url;
+		$args = $this->build_args();
+		$cmd = "wget $args";
+		
+		$pid = $this->run_in_background( $cmd );
+	}
+	
+	function build_args() {
+		
+		$this->log_file = RUN_PATH.$this->url->get_hash();
+		$this->filename = DOWNLOAD_PATH.$this->url->get_hash();
+		
+		$args = array();
+		
+		$args[] = "-b {$url->get_url()}";
+		$args[] = "-0 ".$this->log_file;
+		$args[] = "-o ".$this->filename;
+		
+		try {
+			$host = parse_url( $this->url->get_url(), PHP_URL_HOST );
+			Cookie::exists( $host );
+			$args[] = "--load-cookie ".COOKIE_PATH.$host;
+		} catch ( InvalidCookieException $e ) {}
+		
+		return implode(' ', $args);
+	}
+	
+	function run_in_background( $cmd ) {
+		
+		$error_log = RUN_PATH.$this->url->get_hash().'.errors';
+		return shell_exec("nohup $cmd 2> $error_log & echo $!");
 	}
 }
